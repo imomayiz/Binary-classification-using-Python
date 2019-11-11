24525c12a0b2050560394a231b7a6dc5c46a4da3
i.momayiz <imane.mo07@gmail.com>
2019-11-11 19:07:48 +0100

    test

---
+++ preprocessing.py
@@ -0,0 +1,14 @@
+""""
+Automated script for
+Data cleaning
+Feature selection
+Training/test/scross-validation splitting
+""""
+
+#libraries to import
+import pandas as pd 
+
+#data visualization
+f = open('data_banknote_authentification.csv')
+data = pd.DataFrame(f)
+print(f)
