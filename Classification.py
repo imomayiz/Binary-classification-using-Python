815c5ef6e4dfa649582efc3cd21af888e419b884
Rhum1_n <romain.namyst@imt-atlantique.net>
2019-12-03 21:08:06 +0100

    Add stochastic gradient algorithm + update main

--- Classification.py
+++ Classification.py
@@ -6,6 +6,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+
+
 
 def log_reg(datadict) :
     """
@@ -58,6 +61,23 @@ def SVM(datadict):
     predictions = grid.predict(datadict.get("data_test"))
     return(accuracy_score(predictions, datadict.get("label_test")),grid.best_params_)
     
+def gradient(datadict):
+    """
+    Author: Romain N\n
+    Train and test stochastic gradient algorithm on the dataset contained in datadict
+
+    Args:
+        datadict: dictionary returned by function "preprocessing_main" in Preprocessing.py
+    
+    Returns:
+        Accuracy of stochastic gradient on the testing set, best parameters found during grid search
+    """
+    param_grid={'loss':['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'],'penalty':['none','l2','l1']}
+    grid = GridSearchCV(SGDClassifier(),param_grid,refit=True,cv=5)
+    grid.fit(datadict.get("data_train"),datadict.get("label_train"))
+    pred = grid.predict(datadict.get("data_test"))
+    return(accuracy_score(pred,datadict.get("label_test")),grid.best_params_)
+    
 
 
 if __name__ == '__main__':
@@ -89,6 +109,11 @@ if __name__ == '__main__':
     print("Parameters:",' ')
     print(kd_svm_par)
     print('\n')
+    print("Stochastic Gradient:")
+    print("score: "+str(gradient(kidney)[0]))
+    print("Parameters:",' ')
+    print(gradient(kidney)[1])
+    print('\n')
     print('*'*50)
     
     #BN dataset classification
@@ -107,6 +132,12 @@ if __name__ == '__main__':
     print("score: "+str(bn_svm_score))
     print("Parameters:",' ')
     print(bn_svm_par)
+    print('-'*40)
+    print("Stochastic Gradient:")
+    print("score: "+str(gradient(banknote)[0]))
+    print("Parameters:",' ')
+    print(gradient(kidney)[1])
+    print('\n')
     
     
 #    print("Kidney logistic regression: ", log_reg(kidney))
